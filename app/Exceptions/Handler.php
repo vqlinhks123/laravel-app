5079a3654eb1770da0f3b94e616f96d0e47ebb8a
LinhVN <vuquanglinh2122001@gmail.com>
2023-06-24 10:36:51 +0700

    first commit

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+use Throwable;
+
+class Handler extends \Illuminate\Foundation\Exceptions\Handler
+{
+    // ...
+
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        return parent::render($request, $exception);
+    }
+
+    // ...
+}
